771a64c367fe4672ac8703e53e86da75080cca57
samirneupane12 <91000829+samirneupane12@users.noreply.github.com>
2022-06-22 14:40:46 +0545

    Update data.js

--- data.js
+++ data.js
@@ -1,34 +1,35 @@
 module.exports = {
   mongoLink:
-    "mongodb+srv://samirneupane:jWDjNBceGKbAlGHp@cluster0.zc8za.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://samir:neupane@cluster0.717fr.mongodb.net/?retryWrites=true&w=majority",
 
-  bot_token: "5437789231:AAFeWC12-ZwOEhOs5-Om9GVdaRQNBTL3w0M",
+  bot_token: "5532988087:AAFsQgKeUp1GCKvPIEZHAJcRQzrLAIj7FHM",
 
-  bot_name: "Polygon_Matic_AirdropIbot",
+  bot_name: "sadsdwabot",
 
-  bot_admin: 5383456090,
+  bot_admin: 1774898120,
+  airdropName: "My Deop",
 
   channelscheck: ["@BinanceSmartNews"],
 
-  reffer_bonus: 0.002, // ral bonus amount
+  reffer_bonus: 20000, // ral bonus amount
 
-  min_wd: 0.01,
+  min_wd: 1,
 
-  start_pic: "https://te.legra.ph/file/be01b681711be941cf206.jpg",
+  start_pic: "https://te.legra.ph/file/bab5080924777f244bc68.jpg",
 
-  bot_cur: "WMATIC", //bot cuurency
+  bot_cur: "PolyMetaGon", //bot cuurency
 
-  key: "q88sABWFcZLl6fT4",
+  key: "qMuhK0uM5NjEWyeE",
 
-  privatekey: "ur9UaNqWqAZUI155rV3BCbPEeVKjHigT",
+  privatekey: " sFkxJGgXb8wjqFfN917tZGHj2TjhZj4o",
 
-  cb_account_id: "ddd1fa1e-9cd2-5d4e-a79e-82e52d0781eb",
+  cb_account_id: "2164d53b-d29d-5c45-9719-0aa3a468d30e",
 
   start:
-    "<b>Welcome To WMATIC Airdrop\n\n🎁 Please complete all the tasks and submit details correctly to be eligible for the airdrop\n\n👥 Per Referral : 0.002 WMATIC\n☃️ Withdraw: 0.01WMATIC \n📈 Network : Polygon\n🔐 Distribution: Within 2 Sec \nℹ️ Token Contract :</b> <a href='https://polygonscan.com/token/0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'><b>Wrapped Matic</b></a>\n\n<b>📢 Mandatory Task:-</b>\n🔹Join <a href='https://t.me/BinanceSmartNews'><b>CoinsLooter News</b></a> , <a href='t.me/airdropsdemons'><b>Airdrop Demons</b> </a>  & <a href='t.me/Mr_Airdropsz'><b>Mr Airdropsz</b></a>\n🔹 Join <a href='t.me/MrAirdropsCommunity'><b>MrAirdropsCommunity</b></a> & <a href='t.me/Pshyco_Airdrops'><b>Pshyco_Airdrops</b></a> \n🔹Follow  CoinsLooter <a href='https://twitter.com/coinslootercc'><b>Twitter</b></a> & Mr_Airdropsz <a href='https://twitter.com/Mr_Airdropsz'><b>Twitter</b></a> \n🔹 Join Payment <a href='t.me/payoutfolder'><b>Telegram Channel</b></a>\n\n<i>All the tasks are mandatory And Must Be completed</i>\n\n<b>Click '✅Check' to Procees Further</b>",
+    "<b>Welcome To PolyMetaGon Airdrop\n\n🎁 Please complete all the tasks and submit details correctly to be eligible for the airdrop\n\n👥 Per Referral : 20,000(~20$) PolyMetaGon\n☃️ Withdraw: 100,000(~100$) PolyMetaGon\n📈 Network : Polygon\n🔐 Distribution: Within 2 Sec \nℹ️ Token Contract :</b> <a href='https://polygonscan.com/token/0x6c2d04f536e08AaCf58114d840c3A7bfC51B7E3C'><b>PolyMetaGon</b></a>\n\n<b>📢 Mandatory Task:-</b>\n🔹Join <a href='https://t.me/AirdropsMirror'><b>Airdrops Mirror</b></a> & <a href='t.me/AirdropsDemon'><b>Airdrops Demon</b></a>\n🔹 Join <a href='t.me/unicorn_drops'><b>Unicorn Drops</b></a> & <a href='t.me/AirdropEmail'><b>Airdrops Email</b></a> \n🔹 Subscribe Our <a href='https://youtube.com/channel/UCdYMvnmu0YnNFl_V6Db1L_w'><b>Youtube Channel</b></a>\n🔹 Join Payment <a href='t.me/payoutfolder'><b>Telegram Channel</b></a>\n\n<i>All the tasks are mandatory And Must Be completed</i>\n\n<b>Click '✅Check' to Procees Further</b>",
 
   welcome:
-    "<b><u>🟣 PolyMetaGon [Instant] Airdrop</u></b> 🟣\n\n— Referral Reward : <b>0.002 WMATIC </b>\n— Minimum Withdrawal : <b>0.01 PMG WMATIC</b> \n\n<b>🏆 Winners: All Participants</b>\n<b>⏳ Distribution: Within 2 Sec</b>\n\n📝 Contract Address [PolygonScan]:\n<code>0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270</code>",
+    "<b><u>🟣 PolyMetaGon [Instant] Airdrop</u></b> 🟣\n\n— Referral Reward : <b>20,000(~20$) PMG</b>\n— Minimum Withdrawal : <b>100,000(~100$) PMG</b> \n\n<b>🏆 Winners: All Participants</b>\n<b>⏳ Distribution: Within 2 Sec</b>\n\n📝 Contract Address [PolygonScan]:\n<code>0x6c2d04f536e08AaCf58114d840c3A7bfC51B7E3C</code>",
 
   payment_channel: "@PayoutProofChannel",
 };
